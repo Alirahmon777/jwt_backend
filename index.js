c1fa5cc9018815f4f7f1d87b047094088d6924b4
Alirahmon777 <alirahmonuraliev007@gmail.com>
2023-07-30 15:38:20 +0500

    backenddk

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ import { userRouter } from './src/routers/users.js';
 import { tasksRouter } from './src/routers/tasks.js';
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-app.use(userRouter);
-app.use(tasksRouter);
-
 app.get('/', (req, res) => {
   try {
     res.status(200).send('Welcome');
@@ -18,5 +13,11 @@ app.get('/', (req, res) => {
     res.status(500).send(error.message);
   }
 });
+
+app.use(express.json());
+app.use(cors());
+app.use(userRouter);
+app.use(tasksRouter);
+
 
 app.listen(PORT, () => console.log('Server listening on port: ' + PORT));
