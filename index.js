7ecf84a725b4ccea878b0a72001fc0d5a2b0aa24
Alirahmon777 <alirahmonuraliev007@gmail.com>
2023-07-30 15:40:30 +0500

    backenddko

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import { PORT } from './src/utils/constants.js';
-import { userRouter } from './src/routers/users.js';
-import { tasksRouter } from './src/routers/tasks.js';
+// import { userRouter } from './src/routers/users.js';
+// import { tasksRouter } from './src/routers/tasks.js';
 const app = express();
 
 app.get('/', (req, res) => {
@@ -14,10 +14,10 @@ app.get('/', (req, res) => {
   }
 });
 
-app.use(express.json());
-app.use(cors());
-app.use(userRouter);
-app.use(tasksRouter);
+// app.use(express.json());
+// app.use(cors());
+// app.use(userRouter);
+// app.use(tasksRouter);
 
 
 app.listen(PORT, () => console.log('Server listening on port: ' + PORT));
